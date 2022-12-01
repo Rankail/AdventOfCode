d0386cd09f46a322f9f0ca7e1b72bd1417b6b993
Rankail <thes.samue@gmail.com>
2022-12-01 06:48:14 +0100

    2022 01

--- 2022/01/1.py
+++ 2022/01/1.py
@@ -1,1 +1,13 @@
 with open("i.txt") as f:
+    data = [l.removesuffix("\n") for l in f]
+
+m = 0
+s = 0
+for l in data:
+    if l == "":
+        s = 0
+    else:
+        s += int(l)
+        m = max(s, m)
+
+print(m)
--- 2022/01/2.py
+++ 2022/01/2.py
@@ -1,1 +1,17 @@
-with open("i2.txt") as f:
+with open("i.txt") as f:
+    data = [l.removesuffix("\n") for l in f]
+
+c = []
+m = 0
+s = 0
+for l in data:
+    if l == "":
+        c.append(s)
+        s = 0
+    else:
+        s += int(l)
+        m = max(s, m)
+
+c.sort(reverse=True)
+
+print(c[0]+c[1]+c[2])
