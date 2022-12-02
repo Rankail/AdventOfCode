4add41c5b5c4a0b4511ac0ee200c81ff95cc59d8
Rankail <thes.samue@gmail.com>
2022-12-02 06:30:55 +0100

    2022 02

--- 2022/02/1.py
+++ 2022/02/1.py
@@ -1,1 +1,30 @@
 with open("i.txt") as f:
+    data = [l.removesuffix("\n") for l in f]
+
+n = 0
+for l in data:
+    if not l:
+        continue
+    a=0
+    b = 0
+    if l[0] == "A":
+        a = 0
+    elif l[0] == "B":
+        a = 1
+    elif l[0] == "C":
+        a = 2
+    if l[2] == "X":
+        b = 0
+    elif l[2] == "Y":
+        b = 1
+    elif l[2] == "Z":
+        b = 2
+
+    if b-a == 1 or b-a == -2:
+        n += 6
+    elif b-a == 0:
+        n += 3
+    n+= b+1
+    print (n)
+
+print( n )
--- 2022/02/2.py
+++ 2022/02/2.py
@@ -1,1 +1,23 @@
-with open("i2.txt") as f:
+with open("i.txt") as f:
+    data = [l.removesuffix("\n") for l in f]
+
+n = 0
+for l in data:
+    if not l:
+        continue
+    a=0
+    b = 0
+    if l[0] == "A":
+        a = 0
+    elif l[0] == "B":
+        a = 1
+    elif l[0] == "C":
+        a = 2
+    if l[2] == "X":
+        n += ((a-1+3)%3)+1
+    elif l[2] == "Y":
+        n += a+1+3
+    elif l[2] == "Z":
+        n += 6 + ((a+1)%3)+1
+
+print( n )
