38a81b0a28800899ac3bf105c0884e2639c148ae
Rankail <thes.samue@gmail.com>
2022-12-03 06:31:35 +0100

    2022 03

--- 2022/03/1.py
+++ 2022/03/1.py
@@ -1,1 +1,16 @@
-with open("i.txt") as f:
+import re
+
+data = open("i.txt").read().split("\n")
+
+n = 0
+for d in data:
+    s1 = d[:len(d)//2]
+    s2 = d[len(d)//2:]
+    i = list(set(s1).intersection(s2))[0]
+
+    if ord(i) < 97:
+        n+= ord(i)-65+27
+    else:
+        n+= ord(i)-96
+
+print(n)
--- 2022/03/2.py
+++ 2022/03/2.py
@@ -1,1 +1,13 @@
-with open("i2.txt") as f:
+data = open("i.txt").read().split("\n")
+
+n = 0
+for j in range(0, len(data), 3):
+    i = set(data[j]).intersection(data[j+1])
+    i2 = list(i.intersection(data[j+2]))[0]
+
+    if ord(i2)< 97:
+        n+= ord(i2)-65+27
+    else:
+        n+= ord(i2)-96
+
+print(n)
