84276cda48f40156de9db3f3bc6e5681e35b47ef
Rankail <thes.samue@gmail.com>
2022-12-05 10:34:23 +0100

    2022 05 read input

--- 2022/05/py/1.py
+++ 2022/05/py/1.py
@@ -1,16 +1,21 @@
 import re
-data = open("i.txt").read().split("\n")[10:]
-stacks = ["RPCDBG", "HVG", "NSQDJPM", "PSLGDCNM", "JBNCPFLS", "QBDZVGTS", "BZMHFTQ", "CMDBF", "FCQG"]
+data = open("i.txt").read()
+stackInput, moveInput = data.split("\n\n")
 
-# data = open("i2.txt").read().split("\n")[5:]
-# stacks = ["ZN", "MCD", "P"]
+c = len(stackInput.split("\n")[0])//4+1
+stacks: list[str] = [""]*c
 
-stacks = [list(s) for s in stacks]
+for s in reversed(stackInput.split("\n")[:-1]):
+    i = 0
+    print(s)
+    for i in range(c):
+        if s[i*4+1] != " ":
+            stacks[i] += s[i*4+1]
 
-for l in data:
+for l in moveInput.split("\n"):
     m = re.search(r"move (\d*) from (\d*) to (\d*)", l)
     for i in range(int(m.group(1))):
-        stacks[int(m.group(3))-1].extend(stacks[int(m.group(2))-1][-1:])
+        stacks[int(m.group(3))-1] += stacks[int(m.group(2))-1][-1:]
         stacks[int(m.group(2))-1] = stacks[int(m.group(2))-1][:-1]
 
-print("".join(list(s[-1] for s in stacks)))
+print("".join(s[-1] for s in stacks))
