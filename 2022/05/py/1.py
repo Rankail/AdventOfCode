571f3d100be21f0af87ddb6d116049c0da5653e7
Rankail <thes.samue@gmail.com>
2022-12-05 06:25:36 +0100

    2022 05 py

---
+++ 2022/05/py/1.py
@@ -0,0 +1,17 @@
+import re
+data = open("i.txt").read().split("\n")[10:]
+stacks = ["RPCDBG", "HVG", "NSQDJPM", "PSLGDCNM", "JBNCPFLS", "QBDZVGTS", "BZMHFTQ", "CMDBF", "FCQG"]
+
+# data = open("i2.txt").read().split("\n")[5:]
+# stacks = ["ZN", "MCD", "P"]
+
+stacks = [list(s) for s in stacks]
+
+for l in data:
+    m = re.search(r"move (\d*) from (\d*) to (\d*)", l)
+    print(m.group(1), m.group(2), m.group(3))
+    print(stacks)
+    stacks[int(m.group(3))-1].extend(stacks[int(m.group(2))-1][-int(m.group(1)):])
+    stacks[int(m.group(2))-1] = stacks[int(m.group(2))-1][:-int(m.group(1))]
+
+print("".join(list(s[-1] for s in stacks)))
