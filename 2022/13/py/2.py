f0b552794185badb9df93c9859239ab2b58d2237
Samuel Thesing <thesing@se-rwth.de>
2022-12-13 10:49:51 +0100

    2022 13 py (with eval)

--- 2022/13/py/2.py
+++ 2022/13/py/2.py
@@ -1,33 +1,6 @@
-from typing import Any
 from functools import cmp_to_key
-import re
 
 data = [d for d in open("i.txt").read().split("\n") if d != ""]
-        
-def parse(d: str) -> tuple[list[Any], int]:
-    obj = []
-    token = ""
-    i = 0
-    while i < len(d):
-        e = d[i]
-        if e == ",":
-            if token:
-                obj.append(int(token))
-            token = ""
-        elif e == "]":
-            if token:
-                obj.append(int(token))
-            return (obj, i+1)
-        elif e == "[":
-            n, j = parse(d[i+1:])
-            i += j
-            obj.append(n)
-        else:
-            token += e
-        i += 1
-    if token:
-        obj.append(int(token))
-    return (obj, i)
 
 def compare(l1, l2):
     for l, r in zip(l1, l2):
@@ -51,10 +24,12 @@ def compare(l1, l2):
                 return -1
     return len(l1)-len(l2)
 
-ls = [parse(p[1:-1])[0] for p in data]
+ls = [eval(p) for p in data]
 ls.append([[2]])
 ls.append([[6]])
 ls = sorted(ls, key=cmp_to_key(compare))
 
 ls = [i+1 for i, l in enumerate(ls) if l == [[2]] or l == [[6]]]
+
 print(ls[0]*ls[1])
+# 24948
